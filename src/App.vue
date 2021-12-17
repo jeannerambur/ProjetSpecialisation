bdc90597c95666044868328957f0d29f123283c1
Jeanne Rambur <jeanne_rambur@hotmail.fr>
2021-12-17 17:01:30 +0100

    update style hp

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
-
 <template>
-  <div id="myApplication">
-    <h1>Hello World !</h1>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld'
+
 export default {
-  name: 'app'
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
 <style>
 #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
